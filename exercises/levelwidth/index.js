739c625c163d0514ac8d9529b7239367e3e42ea2
Hui Wang <huiw3939@gmail.com>
2018-02-26 16:51:04 -0500

    binary search trees

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const counters = [0]
+  const arr = [root, "next"]
+  while(arr.length > 1){
+    const node = arr.shift()
+    if(node === 'next'){
+      counters.push(0)
+      arr.push('next')
+    } else {
+      arr.push(...node.children)
+      counters[counters.length - 1]++
+    }
+  }
+  return counters
+}
 
 module.exports = levelWidth;
